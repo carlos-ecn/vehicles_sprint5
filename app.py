47fe1aa7b322f41661a1207c16c5e21c6ea1e890
carlos-ecn <carlos.ecnaka.dsa@gmail.com>
2025-02-22 09:40:09 -0300

    Correções da att 2

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
-%matplotlib inline
 
 carros = pd.read_csv('C:/Users/carlo/Downloads/GIT/Tripleten/vehicles_sprint5/vehicles.csv')
 
@@ -27,30 +24,56 @@ if scat_button:
 
 
 
-# sel_graf = st.checkbox('Selecione o gráfico:')
+# sel_graf = st.selectbox(
+#     'Selecione o gráfico:',
+#     ('Gráfico histogram da coluna odometro', 'Gráfico de dispersão odometro x preço', 'Gráfico histogram ano de fabricação dos carros',
+#     'Gráfico quantidade dos modelos de carros', 'Gráfico de dispersão dos modelos de carros x preço',
+#     'Gráfico de dispersão do ano de fabricação dos carros x preço')
+# )
+
+# def plot_graf(grafico):
+#     if grafico == 'Gráfico histogram da coluna odometro':
+#         fig = px.histogram(carros, x='odometer')
+#     elif grafico == 'Gráfico de dispersão odometro x preço':
+#         fig = px.scatter(carros, x='odometer', y='price')
+#     elif grafico 'Gráfico histogram ano de fabricação dos carros':
+#         fig = px.histogram(carros, x='model_year')
+#     elif grafico 'Gráfico quantidade dos modelos de carros':
+#         fig = px.bar(carros, x='model')
+#     elif grafico 'Gráfico de dispersão dos modelos de carros x preço':
+#         fig = px.scatter(carros, x='model', y='price')
+#     elif grafico 'Gráfico de dispersão do ano de fabricação dos carros x preço':
+#         fig = px.scatter(carros, x='model_year', y='price')
+#     else:
+#         st.warning('Nenhum gráfico selecionado.')
+#         return
+
+#     st.plotly_chart(fig, use_container_width=True)
+
+# plot_graf(sel_graf)
 
 # st.write('Gráfico histogram da coluna odometro')
 # odom_hist = px.histogram(carros, x='odometer')
 # st.plotly_chart(odom_hist, use_container_width=True)
 
 # st.write('Gráfico de dispersão odometro x preço')
-# odom_scatter = px.scatter(main_carros, x='odometer', y='price')
+# odom_scatter = px.scatter(carros, x='odometer', y='price')
 # st.plotly_chart(odom_scatter, use_container_width=True)
 
 # st.write('Gráfico histogram ano de fabricação dos carros')
-# ymodel_hist = px.histogram(main_carros, x='model_year')
+# ymodel_hist = px.histogram(carros, x='model_year')
 # st.plotly_chart(ymodel_hist, use_container_width=True)
 
 # st.write('Gráfico quantidade dos modelos de carros')
-# model_hist = px.bar(main_carros, x='model')
+# model_hist = px.bar(carros, x='model')
 # st.plotly_chart(model_hist, use_container_width=True)
 
 # st.write('Gráfico de dispersão dos modelos de carros x preço')
-# model_scatter = px.scatter(main_carros, x='model', y='price')
+# model_scatter = px.scatter(carros, x='model', y='price')
 # st.plotly_chart(model_scatter, use_container_width=True)
 
 # st.write('Gráfico de dispersão do ano de fabricação dos carros x preço')
-# ymodel_scatter = px.scatter(main_carros, x='model_year', y='price')
+# ymodel_scatter = px.scatter(carros, x='model_year', y='price')
 # st.plotly_chart(ymodel_scatter, use_container_width=True)
 
 
